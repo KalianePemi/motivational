ff6402d762a1dcabb78af16e41e3dee21f97ff20
Kaliane <kaliane.pemi@gmail.com>
2021-11-09 20:48:47 -0300

    adicione imagens

---
+++ app/src/main/java/com/example/Adapter/ItemAdapter.kt
@@ -0,0 +1,14 @@
+
+package com.example.Adapter;
+
+import android.content.Context
+import com.example.affirmations.model.Affirmation;
+
+import java.util.List;
+
+public class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+
+
+
+
+}
